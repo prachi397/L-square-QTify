d048231c2af2e06572c263d8f8a1dd6367602d5d
unknown <prachipanwar397@gmail.com>
2024-10-15 11:33:53 +0530

    carusel component

--- qtify/src/components/Card/Section.js
+++ qtify/src/components/Card/Section.js
@@ -3,9 +3,11 @@ import { Grid, Button, Typography } from "@mui/material";
 import Card from "./Card";
 import axios from "axios";
 import styles from "./Section.module.css";
+import Carousel from "../Carousel/Carousel";
 
 const Section = () => {
   const [cardData, setCardData] = useState([]);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -20,15 +22,33 @@ const Section = () => {
       console.log(err);
     }
   }
+  function handleCollapse() {
+    setIsCollapsed(!isCollapsed);
+  }
 
   return (
     <div className={styles.section}>
-      <Grid container justifyContent="space-between" alignItems="center" className={styles.header}>
-        <Typography variant="h5" color="white">Top Albums</Typography>
-        <Button variant="text" style={{ color: "green", fontWeight: "bold", textTransform:"none" }}>Collapse</Button>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        className={styles.header}
+      >
+        <Typography variant="h5" color="white">
+          Top Albums
+        </Typography>
+        <Button
+          variant="text"
+          style={{ color: "green", fontWeight: "bold", textTransform: "none" }}
+          onClick={handleCollapse}
+        >
+          {isCollapsed ? "Show All": "Collapse"}
+        </Button>
       </Grid>
-
-      <Grid container spacing={3} className={styles.gridContainer}>
+      {isCollapsed ? (
+        <Carousel cardData={cardData}/>
+      ):(
+        <Grid container spacing={3} className={styles.gridContainer}>
         {cardData.length &&
           cardData.map((ele) => (
             <Grid item xs={12} sm={6} md={4} lg={3} key={ele.id}>
@@ -36,6 +56,7 @@ const Section = () => {
             </Grid>
           ))}
       </Grid>
+      )}
     </div>
   );
 };
---
+++ qtify/src/components/Carousel/Carousel.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react"; 
+import "swiper/css"; 
+import "swiper/css/navigation";
+import Card from "../Card/Card";
+import { Navigation } from "swiper/modules";
+
+const Carousel = ({cardData}) =>{
+    return (
+        <Swiper
+          modules={[Navigation]} 
+          slidesPerView={5} 
+          spaceBetween={20}
+          navigation={true}
+          breakpoints={{
+            640: { slidesPerView: 1 }, 
+            768: { slidesPerView: 3 }, 
+            1024: { slidesPerView: 5 },
+          }}
+        >
+          {cardData.map((card) => (
+            <SwiperSlide key={card.id}>
+              <Card data={card} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      );
+}
+export default Carousel;
